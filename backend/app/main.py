79f9ff288660b964911594b683cda70f70fe41e9
seungjin777 <ds020309@naver.com>
2025-07-28 11:45:32 +0900

    feat: 백엔드과제 3단계 수행

--- backend/app/main.py
+++ backend/app/main.py
@@ -41,7 +41,7 @@ def create_mock_data_item(filename, item_id, item_data):
 
 def get_mock_data_item(filename, item_id):
     data = load_mock_data(filename)
-    return data.get(item_id, None)  # ID가 없으면 None 반환
+    return data.get(item_id, None)  # ID가 없으면 None 반환 
 
 def delete_mock_data_item(filename, item_id):
     data = load_mock_data(filename)
@@ -82,12 +82,13 @@ async def custom_404_handler(request: Request, exc: StarletteHTTPException):
 def get_page(request: Request, name: str):
     return templates.TemplateResponse("hello.html", {"request": request, "name": name})
 
+
 # 2단계
 # user.json파일에서 데이터를 받아와 user.html로 넘겨주는 코드를 작성해야함
 @app.get("/users", response_class=JSONResponse)
 def get_users(request: Request):
     with open('./data/users.json') as file:  # users.json파일 불러옴
-      users = json.load(file)
+        users = json.load(file)
     return templates.TemplateResponse("users.html", {"request": request, "users": users})
 
 @app.get("/products", response_class=JSONResponse)
@@ -96,3 +97,32 @@ def get_products(request: Request):
         products = json.load(file)
     return templates.TemplateResponse("products.html", {"request": request, "products": products})
 
+
+# 3단계
+# join 필요(불가능)?, purchases에서 id, user_id로 각각 데이터 추출
+# 이미 작성된 데이터 추출 함수가 있었음
+@app.get("/summary", response_class=JSONResponse)
+def get_purchases(request: Request):
+    
+    # 조합할 데이터 준비
+    purchases = load_mock_data("purchases.json")
+    products = load_mock_data("products.json")
+    users = load_mock_data("users.json")
+    
+    # 조합할 데이터 복제
+    purchases_summary = purchases
+    
+    # purchases에서 user_id와 일치하는 user데이터 삽입
+    # purchases에서 id와 일치하는 product데이터 삽입
+    # o(n^2) 이게 맞나? 예외 처리도 없음
+    for idx, purchase in enumerate(purchases_summary, start=0):
+        for user in users:
+            if(user.get('id', None) == purchase['user_id']):
+                purchases_summary[idx]['user_name'] = user['name']
+                    
+        for product in products:
+            if(product.get('id', None) == purchase['product_id']):
+                purchases_summary[idx]['product_name'] = product['name']
+                purchases_summary[idx]['product_price'] = product['price']
+   
+    return templates.TemplateResponse("summary.html", {"request": request, "purchases": purchases_summary})
